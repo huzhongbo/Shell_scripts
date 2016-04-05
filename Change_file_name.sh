2ec6a4d21af4b6f2803f1da1d186db9be0e6f5cf
ZhongboHu <huzhongbocumt@gmail.com>
2016-04-05 13:39:58 +0200

    modify the Change_file_name.sh script

--- Change_file_name.sh
+++ Change_file_name.sh
@@ -2,9 +2,7 @@
 
 work_dir="/home/user/Desktop/12/"
 
-cd ${work_dir}
-
-for file_a in $(pwd)/*
+for file_a in ${work_dir}/*
 do
     file_name_before=`basename $file_a`
     file_name_after=${file_name_before:0:3}
